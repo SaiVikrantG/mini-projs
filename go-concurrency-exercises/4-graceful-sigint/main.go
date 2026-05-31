9a2e1e0a0857bb10bdddc78a0d22b77bd9eee395
SaiVikrantG <gsvikrant8@gmail.com>
2026-06-01 01:41:44 +0530

    just needed ton detect the sigint signal

---
+++ go-concurrency-exercises/4-graceful-sigint/main.go
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Given is a mock process which runs indefinitely and blocks the
+// program. Right now the only way to stop the program is to send a
+// SIGINT (Ctrl-C). Killing a process like that is not graceful, so we
+// want to try to gracefully stop the process first.
+//
+// Change the program to do the following:
+//   1. On SIGINT try to gracefully stop the process using
+//          `proc.Stop()`
+//   2. If SIGINT is called again, just kill the program (last resort)
+//
+
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// Create a process
+	proc := MockProcess{}
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+
+	go proc.Run()
+
+	<-sigChan
+	go proc.Stop()
+
+	<-sigChan
+	os.Exit(1)
+}
