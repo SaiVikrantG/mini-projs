63c535ac686db9b8a77138edcb3341a9e9a5695c
SaiVikrantG <gsvikrant8@gmail.com>
2026-05-31 17:05:11 +0530

    producer consumer pattern using go routines - mind blown
    helped me form a picture of the order of execution now, and how I can
    make the consumer actually wait for consuming objects the producer
    produces - cool stuff

---
+++ go-concurrency-exercises/1-producer-consumer/main.go
@@ -0,0 +1,52 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Given is a producer-consumer scenario, where a producer reads in
+// tweets from a mockstream and a consumer is processing the
+// data. Your task is to change the code so that the producer as well
+// as the consumer can run concurrently
+//
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var tweets chan *Tweet = make(chan *Tweet)
+
+func producer(stream Stream) {
+	for {
+		tweet, err := stream.Next()
+		if err == ErrEOF {
+			close(tweets)
+			return
+		}
+
+		// tweets = append(tweets, tweet)
+		tweets <- tweet
+	}
+}
+
+func consumer() {
+	for t := range tweets {
+		if t.IsTalkingAboutGo() {
+			fmt.Println(t.Username, "\ttweets about golang")
+		} else {
+			fmt.Println(t.Username, "\tdoes not tweet about golang")
+		}
+	}
+}
+
+func main() {
+	start := time.Now()
+	stream := GetMockStream()
+
+	// Producer
+	go producer(stream)
+
+	// Consumer
+	consumer()
+
+	fmt.Printf("Process took %s\n", time.Since(start))
+}
